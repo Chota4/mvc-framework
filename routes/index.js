f6b2835d4d5e6afb295f551433d9864bc0f0bfe9
Chota4 <rinzinmikeyjr@gmail.com>
2025-03-25 21:16:15 +0600

    Practical 2

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,7 @@
-// Import required modules
 const express = require('express');
-
-// Initialize router
 const router = express.Router();
-
 // Render the landing page
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Online Book Store' });
+res.render('index', { title: 'Online Book Store' });
 });
-
-// Export the router
 module.exports = router;
